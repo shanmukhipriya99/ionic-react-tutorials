f4ae85fdbe5273999318430f22b86b94d6cd43b0
Shanmukhi Priya <shanmukhipriya99@gmail.com>
2022-08-09 23:07:07 -0400

    complete ionic tags

--- section-2/hello-world/src/App.js
+++ section-2/hello-world/src/App.js
@@ -1,26 +1,53 @@
-import {IonButton, IonIcon, IonToast} from '@ionic/react';
-import { play as playIcon} from "ionicons/icons"; //setting alias using "as"
-import React, {useState} from 'react';
+import {
+  IonApp,
+  IonButton,
+  IonContent,
+  IonHeader,
+  IonIcon,
+  IonLabel,
+  IonTitle,
+  IonToast,
+  IonToolbar,
+} from "@ionic/react";
+import { play as playIcon } from "ionicons/icons"; //setting alias using "as"
+import React, { useState } from "react";
 
 function App() {
   const [showToast, setShowToast] = useState(false);
   const handleClick = () => {
     setShowToast(true);
     setTimeout(() => setShowToast(false), 1500);
-  }
+  };
   return (
-    <div>
-      <header>
-        <h1>My App</h1>
-      </header>
-      <main>
-        <p>Add some content here…</p>
+    // <div>
+    //   <header>
+    //     <h1>My App</h1>
+    //   </header>
+    //   <main>
+    //     <p>Add some content here…</p>
+    //     <IonButton onClick={handleClick}>
+    //       <IonIcon icon={playIcon} slot="start" />{" "}
+    //       {/*other values for slot: start, end, icon-only */}
+    //       Click here!
+    //     </IonButton>
+    //     <IonToast isOpen={showToast} message="Hello World!" />
+    //   </main>
+    // </div>
+    <IonApp>
+      <IonHeader>
+        <IonToolbar>
+          <IonTitle>My App</IonTitle>
+        </IonToolbar>
+      </IonHeader>
+      <IonContent class="ion-padding">
         <IonButton onClick={handleClick}>
-        <IonIcon icon={playIcon} slot="start" /> {/*other values for slot: start, end, icon-only */}
-        Click here!</IonButton>
+          <IonIcon icon={playIcon} slot="start" />{" "}
+          {/*other values for slot: start, end, icon-only */}
+          Click here!
+        </IonButton>
         <IonToast isOpen={showToast} message="Hello World!" />
-      </main>
-    </div>
+      </IonContent>
+    </IonApp>
   );
 }
 
