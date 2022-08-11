3cbf923f0d31b891e0ba07437e0bddbd7fd5630d
Shanmukhi Priya <shanmukhipriya99@gmail.com>
2022-08-11 13:00:29 -0400

    formatted date

--- section-3/biorhythm-calculator/src/components/BiorhythmCard.js
+++ section-3/biorhythm-calculator/src/components/BiorhythmCard.js
@@ -1,11 +1,16 @@
 import { IonCard, IonCardContent, IonCardHeader, IonCardTitle } from "@ionic/react";
 import React from "react";
+import dayjs from "dayjs";
+
+function dateFormat(isoString) {
+return (dayjs(isoString).format('MMM DD YYYY'));
+}
 
 function BiorhythmCard ({ targetDate }) {
 return (
     <IonCard className="ion-text-center">
           <IonCardHeader>
-            <IonCardTitle>{targetDate}</IonCardTitle>
+            <IonCardTitle>{dateFormat(targetDate)}</IonCardTitle>
           </IonCardHeader>
           <IonCardContent>
             <p>Physical: xx%</p>
